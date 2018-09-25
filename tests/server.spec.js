73bb1a5f78d72804241a365c8d214bb75fba44fa
Ivan Reynoso <IvanReynoso@Ivans-MacBook-Pro.local>
2018-09-25 10:50:20 -0700

    First commit

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,16 @@ describe('server/app.js', function() {
       done();
     });
   });
+  
+  it('page says test', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('test');
+        done();
+      });
+    });
+
+
 })
